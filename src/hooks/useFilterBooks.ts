c2ef5d0e9100f33394bc65e11c61b41614f26317
Maxim Kuzin <iskait04@gmail.com>
2023-04-25 20:03:06 +0300

    Миграция на Vite #3

--- src/hooks/useFilterBooks.ts
+++ src/hooks/useFilterBooks.ts
@@ -3,7 +3,7 @@ import useAppSelector from "./useAppSelector";
 export default function useFilterBooks() {
   const { items, category } = useAppSelector((state) => state.books);
   return items.filter(
-    ({ volumeInfo }, idx, arr) =>
+    ({ volumeInfo }) =>
       volumeInfo.categories?.includes(category) || category === "All"
   );
 }
