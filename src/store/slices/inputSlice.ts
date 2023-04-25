1dcedfad275783217f8fb329d2877dd311fc2842
Maxim Kuzin <iskait04@gmail.com>
2023-04-25 21:58:05 +0300

    Исправление ошибок билда #11

--- src/store/slices/inputSlice.ts
+++ src/store/slices/inputSlice.ts
@@ -4,8 +4,10 @@ const inputSlice = createSlice({
   name: "input",
   initialState: "" as string,
   reducers: {
-    setInput: (state, action: PayloadAction<string>) =>
-      (state = action.payload),
+    setInput(state, action: PayloadAction<string>) {
+      state = action.payload;
+      return state;
+    },
   },
 });
 
